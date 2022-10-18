134a0a43d3e286b1ca2a9e1b975dba9c50ff70ca
Alina <ram@sfedu.ru>
2022-10-19 00:28:03 +0300

    -

---
+++ fractals/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace fractals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.iteration_count = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(224, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(564, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // iteration_count
+            // 
+            this.iteration_count.Location = new System.Drawing.Point(190, 35);
+            this.iteration_count.Name = "iteration_count";
+            this.iteration_count.Size = new System.Drawing.Size(28, 22);
+            this.iteration_count.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Кол-во итераций:";
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.domainUpDown1.Items.Add("Куст 1");
+            this.domainUpDown1.Items.Add("Куст 2");
+            this.domainUpDown1.Items.Add("Кривая Коха");
+            this.domainUpDown1.Items.Add("Снежинка Коха");
+            this.domainUpDown1.Location = new System.Drawing.Point(12, 73);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(156, 24);
+            this.domainUpDown1.TabIndex = 4;
+            this.domainUpDown1.Text = "Тип фрактала";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iteration_count);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox iteration_count;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ fractals/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Laba4.Primitives;
+
+namespace fractals
+{   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+        }
+
+        char aksiom;
+        float corner;
+        float direction;
+        List<string[]> rules;
+
+        private Graphics g;
+        private List<PointApp> points = new List<PointApp>();
+        private List<Line> lines = new List<Line>();
+
+
+        private List<PointApp> Rotate(float alpha)
+        {
+            alpha = (float)((Math.PI * alpha) / 180);
+            float[] center = new float[3];
+            List<PointApp> RotateList = new List<PointApp>();
+
+            foreach (PointApp point in points)
+            {
+                center[0] += point.X;
+                center[1] += point.Y;
+            }
+            center[0] /= points.Count();
+            center[1] /= points.Count();
+            center[2] = 0;
+
+            float[] RotateMatrix = new float[9] { (float)Math.Cos(alpha),         (float)Math.Sin(alpha),    0,
+                                                      (float)Math.Sin(alpha) * -1,    (float)Math.Cos(alpha),    0,
+                                                      center[0],                      center[1],                 1 };
+
+
+
+            foreach (PointApp point in points)
+            {
+                float[] point_matrix = new float[3] { point.X, point.Y, 1 };
+                float[] res_point = new float[2];
+
+                for (int i = 0; i < 2; i += 1)
+                {
+                    for (int j = 0; j < 3; j += 1)
+                    {
+                        res_point[i] += (point_matrix[j] - center[j]) * RotateMatrix[i + j * 3];
+                    }
+                }
+
+                RotateList.Add(new PointApp(res_point[0], res_point[1]));
+            }
+
+            return RotateList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filename = "";
+            if (domainUpDown1.SelectedItem.Equals("Куст 1"))
+            {
+                filename = "files/bush_1.txt";
+            }
+            else if (domainUpDown1.SelectedItem.Equals("Куст 2"))
+            {
+                filename = "files/bush_2.txt";
+            }
+            else if (domainUpDown1.SelectedItem.Equals("Куст 3"))
+            {
+                filename = "files/bush_3.txt";
+            }
+            else if (domainUpDown1.SelectedItem.Equals("Снежинка Коха"))
+            {
+                filename = "files/kohs_nowflake.txt";
+            }
+            else if (domainUpDown1.SelectedItem.Equals("6-тиугольная мозаика"))
+            {
+                filename = "files/6.txt";
+            }
+            else if (domainUpDown1.SelectedItem.Equals("Дерево"))
+            {
+                filename = "files/tree.txt";
+            }
+            else if (domainUpDown1.SelectedItem.Equals("Кривая Коха"))
+            {
+                filename = "files/kohs_curved.txt";
+            }
+            
+
+           StreamReader f = new StreamReader(filename);
+            List<string> s = f.ReadLine().Split().ToList();
+
+            if(s[0].Length == 1)
+                aksiom = char.Parse(s[0]);
+            else//если аксиома - не один элемент
+                aksiom = s[0][0];
+
+            corner = float.Parse(s[1]);
+            direction = float.Parse(s[2]);
+
+            rules = new List<string[]>();
+            while (!f.EndOfStream)
+            {
+                rules.Add(f.ReadLine().Split());
+            }
+            f.Close();
+
+            Paint_fractal(rules);
+        }
+
+        PointApp end;
+        int line_lenght = 50;
+        int iter = 0;
+        int step = 0;
+        public void Paint_fractal(List<string[]> rules)
+        {
+            
+            // правило аксиомы
+            string rule = rules.Where(x => char.Parse(x[0]) == aksiom).ToList()[0][2];
+
+            for (int i = 0; i < rule.Length; i++)
+            {
+                if (rule[i] == aksiom)
+                {
+                    PointApp p1 = step == 0 ? new PointApp(100, pictureBox1.Height - 100) : end;
+                    PointApp p2 = new PointApp(p1.X + line_lenght, p1.Y);
+                    points.Add(p2);
+                    p2 = Rotate(direction).First();
+                    end = p2;
+                    points.Clear();
+                    Line line = new Line(p1, p2);
+                    lines.Add(line);
+
+                    step++;
+                }
+                else if (rule[i] == '+')
+                {
+                    direction += corner;
+                }
+                else if (rule[i] == '-')
+                {
+                    direction -= corner;
+                }
+                else if (rule[i] == '[')
+                {
+
+                }
+                else if (rule[i] == ']')
+                {
+
+                }
+                else // переменная - неаксиома
+                {
+                    rule = rules.Where(x => char.Parse(x[0]) == rule[i]).ToList()[0][2];
+                    recurs(rule, rule[i]);
+                }
+            }
+
+            iter++;
+            if (iter != int.Parse(iteration_count.Text))
+                Paint_fractal(rules);
+
+            this.lines.ForEach((p) => p.Draw(g));
+
+            rules.Clear();
+        }
+
+        public void recurs(string rule, char symbol)
+        {
+            for (int i = 0; i < rule.Length; i++)
+            {
+                if (rule[i] == symbol)
+                {
+                    PointApp p1 = iter == 0 ? new PointApp(100, pictureBox1.Height - 100) : end;
+                    PointApp p2 = new PointApp(p1.X + line_lenght, p1.Y);
+                    points.Add(p2);
+                    p2 = Rotate(direction).First();
+                    end = p2;
+                    points.Clear();
+                    Line line = new Line(p1, p2);
+                    line.Draw(g);
+
+                    step++;
+                }
+                else if (rule[i] == '+')
+                {
+                    direction += corner;
+                }
+                else if (rule[i] == '-')
+                {
+                    direction -= corner;
+                }
+                else if (rule[i] == '[')
+                {
+
+                }
+                else if (rule[i] == ']')
+                {
+
+                }
+                else // другая переменная
+                {
+                    rule = rules.Where(x => char.Parse(x[0]) == rule[i]).ToList()[0][2];
+                    recurs(rule, rule[i]);
+                }
+            }
+        }
+
+        
+    }
+}
